7f284ed00522e54836864326dbc5b97634f229f7
monosodrac <monosodrac@gmail.com>
2025-09-09 17:12:06 -0300

    create Footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Container = styled.footer`
+  padding: 32px 0;
+  background-color: ${cores.cinza};
+  font-size: 14px;
+`
+
+export const SectionTitle = styled.h4`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${cores.branca};
+`
+
+export const Links = styled.ul`
+  display: flex;
+  margin-top: 16px;
+`
+
+export const Link = styled.a`
+  margin-right: 8px;
+  color: ${cores.cinzaClaro};
+  text-decoration: none;
+`
+
+export const FooterSection = styled.div`
+  margin-bottom: 64px;
+`
