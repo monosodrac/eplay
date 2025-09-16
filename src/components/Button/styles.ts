522c2a37d7d609ce50ad736bc11645bc721b2175
monosodrac <monosodrac@gmail.com>
2025-09-16 11:53:29 -0300

    create hero to second page; put a black div to overlay the banner and turn down the opacity to make a banner with dark overlay; use display: flex with flex-direction: column to set a vertical align of the components in Hero, and separate them with justify-content: space-between

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 
 import { cores } from '../../styles'
 
-export const ButtonContainer = styled.button`
+import { Props } from '.'
+
+export const ButtonContainer = styled.button<Props>`
   padding: 8px 16px;
-  border: 2px solid ${cores.branca};
+  border: 2px solid
+    ${(props) => (props.variant === 'primary' ? cores.verde : cores.branca)};
   border-radius: 8px;
   font-size: 16px;
   font-weight: bold;
-  background-color: transparent;
+  background-color: ${(props) =>
+    props.variant === 'primary' ? cores.verde : 'transparent'};
   color: ${cores.branca};
 `
 
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
+
+export const Banner = styled.div`
+  position: relative;
+  display: block;
+  height: 480px;
+  width: 100%;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  padding-top: 16px;
+
+  &::after {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #000;
+    content: '';
+    opacity: 0.56;
+  }
+
+  ${TagContainer} {
+    margin-right: 8px;
+  }
+
+  .container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    z-index: 1;
+  }
+`
+
+export const Infos = styled.div`
+  padding: 16px;
+  max-width: 290px;
+  font-weight: bold;
+  background-color: ${cores.preta};
+
+  h2 {
+    font-size: 32px;
+  }
+
+  p {
+    margin: 16px 0;
+    font-size: 18px;
+
+    span {
+      display: block;
+      text-decoration: line-through;
+    }
+  }
+`
