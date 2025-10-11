9680d25ecde6adb2228fbfff0d31365033feb14b
monosodrac <monosodrac@gmail.com>
2025-10-11 13:51:26 -0300

    create hamburguer menu in tablet screen breakpoint

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,38 +1,77 @@
 import styled from 'styled-components'
 import { Breakpoints, cores } from '../../styles'
 
-export const HeaderBar = styled.header`
+export const Links = styled.ul`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  margin-left: 40px;
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    display: block;
+    margin-left: 0;
+  }
+`
+
+export const HeaderBar = styled.header`
   padding: 24px;
   margin-bottom: 80px;
   border-radius: 16px;
   background-color: ${cores.cinza};
-
-  div {
-    display: flex;
-    align-items: center;
-  }
 
   a {
     color: ${cores.branca};
     text-decoration: none;
     font-weight: bold;
   }
+`
 
-  @media (max-width: ${Breakpoints.tablet}) {
-    display: none;
+export const HeaderRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    @media (max-width: ${Breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+
+      ${Links} {
+        display: none;
+      }
+    }
   }
 `
 
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  @media (max-width: ${Breakpoints.desktop}) {
+    &.is-open {
+      display: none;
+    }
+  }
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    &.is-open {
+      display: block;
+    }
+  }
 `
 
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -40,5 +79,27 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+
+  span {
+    display: block;
+    margin-bottom: 4px;
+    height: 2px;
+    width: 100%;
+    background-color: ${cores.branca};
+  }
+
+  @media (min-width: ${Breakpoints.tablet}) {
+    display: none;
   }
 `
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,13 +1,29 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  align-items: flex-end;
+  margin-top: ${(props) => props.marginTop || '0'};
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     display: block;
@@ -15,11 +31,29 @@ export const InputGroup = styled.div`
     font-size: 14px;
   }
 
-  input {
+  input,
+  select {
     padding: 0 8px;
     border: 1px solid ${cores.branca};
     height: 32px;
     width: 100%;
     background-color: ${cores.branca};
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  padding: 0 8px;
+  margin-right: 16px;
+  border: none;
+  border-radius: 8px;
+  height: 32px;
+  font-weight: bold;
+  font-size: 14px;
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  color: ${cores.branca};
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
